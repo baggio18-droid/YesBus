81855a0a61dee0bd9f3af04c42a21af8bf685d47
baggio18-droid <derogerbaggio19@gmail.com>
2021-06-04 23:19:13 +0700

    authentication & fill order form(undone)

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function home(){
-        return view('pages.home');
+       return view('pages.home');
     }
 
     public function destination(){
